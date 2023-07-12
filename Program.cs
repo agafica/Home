02495c3b9d268ada575edde953b4eaeec555d7f6
Albina <sagafio@mail.ru>
2023-07-12 21:26:03 +0300

    Task2

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// Task1.Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+/*
+int CutNumber(int num)
+{
+    int hundred = num/100;
+    int units = num % 100;
+
+    int result = (units - (num % 10));
+    return result;
+}
+
+int randNumber = new Random().Next(100,1000);
+int newNumber = CutNumber(randNumber);
+
+Console.WriteLine($"New version of {randNumber} is {newNumber}");*/
+
+
+ // Task2. Напишите программу, которая выводит третью цифру заданного числа.
+
+/*int CutNumber(int num)
+{
+    int hundred = num/100;
+    int units = num % 10;
+
+    int result = (units);
+    return result;
+}
+
+int randNumber = new Random().Next(100,1000);
+int newNumber = CutNumber(randNumber);
+
+Console.WriteLine($"New version of {randNumber} is {newNumber}");*/
+
+
